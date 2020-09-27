9662a3d6741766a6921c90566e9a45b012da56e2
Michael Handkins <michaelhandkins@gmail.com>
2020-09-27 13:45:13 -0500

    Create functions that store the user entered textfield text in an array called lyrics

--- topline/Controller/NoteViewController.swift
+++ topline/Controller/NoteViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 import RealmSwift
 
-class NoteViewController: UITableViewController {
+class NoteViewController: UITableViewController, UITextFieldDelegate {
     
     let realm = try! Realm()
-    var note: [String] = []
+    var lyrics: [String] = []
     var noteTitle: String = ""
+    var notes: [Note]? = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +25,36 @@ class NoteViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        if notes != nil {
+            return notes!.count + 1
+        } else {
+            return 1
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "lyricsCell", for: indexPath) as! newNoteTableViewCell
+        
+        cell.lyricsField.delegate = self
 
         return cell
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textFieldDidEndEditing(textField)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if textField.text != "" {
+            
+            lyrics.append(textField.text!)
+            
+        }
+        
+        
+        
     }
 
 }
